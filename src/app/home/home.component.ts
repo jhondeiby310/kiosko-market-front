5c52c72812cc81bfc1c613bba55d869109c91b1f
brujordi <jordilomejor@gmail.com>
2020-06-21 19:09:53 -0500

    Creacion CRUD categorias

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,15 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { ApiServiceP } from '../core/services/api.serviceproducts';
+import Producto from '../core/models/producto.model';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.css'],
+  providers: [ApiServiceP]
 })
 export class HomeComponent implements OnInit {
 
-  constructor() { }
+  constructor(private apiProductos: ApiServiceP) { }
+  productos: Producto[]
 
   ngOnInit() {
+    this.apiProductos.obtenerProductos().subscribe(
+      (res: any) => this.productos = res,
+      err => console.log(err)
+    )
   }
 
 }
